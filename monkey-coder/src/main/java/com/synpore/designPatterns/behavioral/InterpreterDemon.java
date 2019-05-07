19998763bb3da6c11199ac1a23d31c9c41bd8c09
hurenxian <hurenxian@iqianjin.com>
2019-05-07 18:50:54 +0800

    inteprter and iterator design pattern

---
+++ monkey-coder/src/main/java/com/synpore/designPatterns/behavioral/InterpreterDemon.java
@@ -0,0 +1,6 @@
+package com.synpore.designPatterns.behavioral;
+
+//Given a language, define a representation for its grammar along with an
+// interpreter that uses the representation to interpret sentences in the language.
+public class InterpreterDemon {
+}
---
+++ monkey-coder/src/main/java/com/synpore/designPatterns/behavioral/IteratorDemon.java
@@ -0,0 +1,123 @@
+package com.synpore.designPatterns.behavioral;
+
+//Provide a way to access the elements of an aggregate object sequentially without exposing its underlying representation.
+public class IteratorDemon {
+    public static void main(String[] args) {
+        Object[] objArray = {"One","Two","Three","Four","Five","Six"};
+        //创建聚合对象
+        Aggregate agg = new ConcreteAggregate(objArray);
+        //循环输出聚合对象中的值
+        Iterator it = agg.createIterator();
+        while(!it.isDone()){
+            System.out.println(it.currentItem());
+            it.next();
+        }
+    }
+}
+abstract class Aggregate {
+    /**
+     * 工厂方法，创建相应迭代子对象的接口
+     */
+    public abstract Iterator createIterator();
+}
+
+class ConcreteAggregate extends Aggregate {
+
+    private Object[] objArray = null;
+    /**
+     * 构造方法，传入聚合对象的具体内容
+     */
+    public ConcreteAggregate(Object[] objArray){
+        this.objArray = objArray;
+    }
+
+    @Override
+    public Iterator createIterator() {
+
+        return new ConcreteIterator(this);
+    }
+    /**
+     * 取值方法：向外界提供聚集元素
+     */
+    public Object getElement(int index){
+
+        if(index < objArray.length){
+            return objArray[index];
+        }else{
+            return null;
+        }
+    }
+    /**
+     * 取值方法：向外界提供聚集的大小
+     */
+    public int size(){
+        return objArray.length;
+    }
+}
+interface Iterator {
+    /**
+     * 迭代方法：移动到第一个元素
+     */
+    public void first();
+    /**
+     * 迭代方法：移动到下一个元素
+     */
+    public void next();
+    /**
+     * 迭代方法：是否为最后一个元素
+     */
+    public boolean isDone();
+    /**
+     * 迭代方法：返还当前元素
+     */
+    public Object currentItem();
+}
+
+class ConcreteIterator implements Iterator {
+    //持有被迭代的具体的聚合对象
+    private ConcreteAggregate agg;
+    //内部索引，记录当前迭代到的索引位置
+    private int index = 0;
+    //记录当前聚集对象的大小
+    private int size = 0;
+
+    public ConcreteIterator(ConcreteAggregate agg){
+        this.agg = agg;
+        this.size = agg.size();
+        index = 0;
+    }
+    /**
+     * 迭代方法：返还当前元素
+     */
+    @Override
+    public Object currentItem() {
+        return agg.getElement(index);
+    }
+    /**
+     * 迭代方法：移动到第一个元素
+     */
+    @Override
+    public void first() {
+
+        index = 0;
+    }
+    /**
+     * 迭代方法：是否为最后一个元素
+     */
+    @Override
+    public boolean isDone() {
+        return (index >= size);
+    }
+    /**
+     * 迭代方法：移动到下一个元素
+     */
+    @Override
+    public void next() {
+
+        if(index < size)
+        {
+            index ++;
+        }
+    }
+
+}
