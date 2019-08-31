41ac708cabbc1be143b68adcb362985d0a97b396
hurenxian <hurenxian@xueqiu.com>
2019-08-31 22:55:29 +0800

    test3-1

--- monkey-coder/src/main/java/com/synpore/baseSource/ArrayListDemo.java
+++ monkey-coder/src/main/java/com/synpore/baseSource/ArrayListDemo.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class ArrayListDemo {
+    //
 
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<Integer>();
